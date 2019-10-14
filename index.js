b0fa421cc2f7dbcb07ce24fcaccc7749e8605bb2
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-10-14 12:56:23 -0500

    worked on protected middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const bcrypt = require('bcryptjs')
+
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
 
@@ -18,6 +20,10 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  const hash = bcrypt.hashSync(user.password, 8)
+
+  user.password = hash
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -33,7 +39,9 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
+    
+
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -44,7 +52,7 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -56,10 +64,31 @@ server.get('/hash', (req, res) => {
   // read a password from the Authorization header
   // return an object with the password hashed using bcryptjs
   // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
-  console.log(req.headers)
-  var hash = bcrypt.hashSync(req.headers.password)
-  res.status(200).json(hash)
+  const password = req.headers.password;
+
+
+  const hash = bcrypt.hashSync(password, 8)
+  res.status(200).json({ hash })
 })
+
+// implement the protected middleware that will check for username and password
+// in the headers and if valid provide access to the endpoint
+function protected(req, res, next) {
+  const {username, password} = req.headers
+
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (bcrypt.compareSync(password, user.password)) {
+        next()
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
